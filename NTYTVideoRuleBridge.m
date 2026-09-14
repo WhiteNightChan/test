8cb6ef9ee22528650cd531b7c0c0119019fa960e
WhiteNightChan <58456512+WhiteNightChan@users.noreply.github.com>
2026-09-14 10:52:10 +0900

    2g

--- NTYTVideoRuleBridge.m
+++ NTYTVideoRuleBridge.m
@@ -38,7 +38,7 @@ static NSArray<NTYTRule *> *NTYTPhase4BlockRules(void) {
          */
         NTYTRuleParserResult *result =
             [NTYTRuleParser
-                parseSource:@"自作PC"
+                parseSource:@"自作PC $& ニュース"
                 sourceSection:@"general.block"
                 baseField:NTYTFieldGeneralText
                 identifierPrefix:@"phase4.general.block"
